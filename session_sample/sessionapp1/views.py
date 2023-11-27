7de58e797d9528eeab6ccf373494f5bb056d3ba8
sharafunneesa <sharafunneesa111.pp@gmail.com>
2023-11-27 20:46:53 +0530

    modified

--- session_sample/sessionapp1/views.py
+++ session_sample/sessionapp1/views.py
@@ -14,33 +14,13 @@ def create(request):
         else:
             frm=MovieForm()    
 
-        # title=(request.POST.get('title'))
-        # year=request.POST.get('year')
-        # desc=request.POST.get('description')
-        # movie_obj=movies(title=title,year=year,description=desc)
-        # movie_obj.save()
+       
+       
 
 
     return render(request,'create.html',{'frm':frm})
-# def list(request):
-
-#     # movie_set=movies.objects.all()
-#     movie_set=movies.objects.filter(year=1990)
-#     print(movie_set)
-#     return render(request,'list.html',{'movies':movie_set})
 
 
-# how cookies work
-# def list(request):
-#     print(request.COOKIES)
-#     visits=int(request.COOKIES.get('visits',0))
-#     visits=visits+1
-#     # movie_set=movies.objects.all()
-#     movie_set=movies.objects.filter(year=1990)
-#     print(movie_set)
-#     response=render(request,'list.html',{'movies':movie_set,'visits':visits})
-#     response.set_cookie('visits',visits)
-#     return response
 
 
 # HOW SESSION WORKS
@@ -64,19 +44,8 @@ def list(request):
 
 
 
-# def edit(request,pk):
-#     instance_to_be_edited = movies.objects.get(pk=pk)
-#     if request.POST:
-#         frm=MovieForm(request.POST,instance=instance_to_be_edited)
-#         if frm.is_valid():
-#             instance_to_be_edited.save()
-#     else:
-#         frm=MovieForm(instance=instance_to_be_edited)
-#     return render(request,'create.html',{'frm':frm})
 
 
-
-# using session
 
 def edit(request,pk):
     
@@ -93,20 +62,8 @@ def edit(request,pk):
         frm=MovieForm(instance=instance_to_be_edited)
     return render(request,'create.html',{'frm':frm})
 
-        # title=request.POST.get('title')
-        # year=request.POST.get('year')
-        # description=request.POST.get('description')
-        # instance_to_be_edited.title=title
-        # instance_to_be_edited.year=year
-        # instance_to_be_edited.description=description
-        # instance_to_be_edited.save()
-
-
-
-    
-    
-    # return render(request,'edit.html')
-
+       
+       
 
 
 def delete(request,pk):
